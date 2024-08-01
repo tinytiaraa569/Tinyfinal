d5a76d6ea1db8091ff20f903d4e1d25098816091
tinytiaraa569 <tinytiaraa569@gmail.com>
2024-08-01 12:08:50 +0530

    Update server.jsx

--- tinytiaraa/client/src/server.jsx
+++ tinytiaraa/client/src/server.jsx
@@ -1,4 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://tinytiaraa.vercel.app/api/v2";
 
 
-export const backend_url = "http://localhost:8000/"
+export const backend_url = "https://tinytiaraa.vercel.app/"
