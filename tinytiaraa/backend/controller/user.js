87a511e9162b703eaa627c502adb44615fc4dd45
tinytiaraa569 <tinytiaraa569@gmail.com>
2024-08-02 11:49:17 +0530

    Update user.js

--- tinytiaraa/backend/controller/user.js
+++ tinytiaraa/backend/controller/user.js
@@ -20,7 +20,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 
         if (userEmail) {
             const filename = req.file.filename
-            const filePath = `/uploads/${filename}`
+            const filePath = `https://tiny-tiaraa.vercel.app/uploads/${filename}`
             fs.unlink(filePath, (err) => {
                 if (err) {
                     console.log(err)
