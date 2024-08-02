6987f528234f927fcccb1a6e98ce42148f825997
tinytiaraa569 <tinytiaraa569@gmail.com>
2024-08-02 11:42:42 +0530

    changes doness

--- tinytiaraa/backend/controller/user.js
+++ tinytiaraa/backend/controller/user.js
@@ -40,7 +40,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
             name: name,
             email: email,
             password: password,
-            // avatar: fileUrl,
+            avatar: fileUrl,
         }
         const activationToken = createActivationToken(user)
         const activationUrl = `https://tiny-tiaraa.vercel.app/activation/${activationToken}`
