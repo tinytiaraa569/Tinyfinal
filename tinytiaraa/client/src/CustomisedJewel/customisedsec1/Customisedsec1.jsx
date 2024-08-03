0b5fda0029c811c12ead71c3f96935d5f51acc0c
tinytiaraa569 <tinytiaraa569@gmail.com>
2024-08-03 13:28:38 +0530

    product custom banner changed

--- tinytiaraa/client/src/CustomisedJewel/customisedsec1/Customisedsec1.jsx
+++ tinytiaraa/client/src/CustomisedJewel/customisedsec1/Customisedsec1.jsx
@@ -4,9 +4,9 @@ import './Customised1.css'
 function Customisedsec1() {
   return (
     <div className='Customisedsec1'>
-        <div className="customisedcon1">
+        {/* <div className="customisedcon1">
             <h1>Design Your Own Masterpiece</h1>
-        </div>
+        </div> */}
         
     </div>
   )
