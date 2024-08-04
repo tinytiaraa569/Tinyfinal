9fcb04204fd27c98e1c39c4137483e07a864e613
tinytiaraa569 <tinytiaraa569@gmail.com>
2024-08-04 21:25:27 +0530

    Update app.js

--- tinytiaraa/backend/app.js
+++ tinytiaraa/backend/app.js
@@ -11,7 +11,9 @@ const path = require("path")
 const app = express()
 
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+
 app.use(cookieParser());
 
 
