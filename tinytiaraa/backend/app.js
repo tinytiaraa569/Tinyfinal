73e812005fb6d1ded3905d84081bc4b211ee3631
tinytiaraa569 <tinytiaraa569@gmail.com>
2024-08-06 10:34:25 +0530

    Update app.js

--- tinytiaraa/backend/app.js
+++ tinytiaraa/backend/app.js
@@ -13,14 +13,15 @@ const app = express()
 
 // app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true, parameterLimit:10000000,limit: "100mb" }));
-app.use(express.json({ limit: '100mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 
 app.use(cors({
     origin: 'https://tiny-tiaraa.vercel.app',
     credentials: true
 }));
+app.use(express.json());
 
 
 app.use("/test", (req, res) => {
