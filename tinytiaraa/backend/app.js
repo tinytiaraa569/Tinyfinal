493f86569da61a0ec3296313d40cedd99ca4bffa
tinytiaraa569 <tinytiaraa569@gmail.com>
2024-08-06 12:32:26 +0530

    Update app.js

--- tinytiaraa/backend/app.js
+++ tinytiaraa/backend/app.js
@@ -19,9 +19,8 @@ app.use(cors({
 
 // app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ limit: "50mb" , extended: true , parameterLimit:50000}));
+app.use(bodyParser.json({ limit: '50mb' }));
 
 
 
